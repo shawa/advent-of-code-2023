a2f4de2b91fcd016f49bce351bf37610013dcf91
shawa <shawa@localhost>
2023-12-08 17:53:05 +0000

    'fix' day 7

--- lib/advent2023/day_07.ex
+++ lib/advent2023/day_07.ex
@@ -2,19 +2,10 @@ defmodule Advent2023.Day07 do
   def part_one(input) do
     input
     |> Enum.map(&parse_line/1)
-    |> Enum.sort_by(fn {hand, _wager} -> classify_hand(hand) end)
+    |> Enum.map(fn {hand, bid} -> {Enum.map(hand, &rank_card(&1, jacks: :high)), bid} end)
+    |> Enum.sort_by(&classify_hand(&1, jacks: :normal))
     |> Enum.with_index(1)
     |> Enum.map(fn {{_hand, wager}, i} -> wager * i end)
-    |> Enum.sum()
-  end
-
-  def part_two(input) do
-    input
-    |> Enum.map(&parse_line/1)
-    |> Enum.map(&find_most_jacked_hand/1)
-    |> Enum.sort_by(&most_jacked/1)
-    |> Enum.with_index(1)
-    |> Enum.map(fn {{_, wager}, i} -> wager * i end)
     |> Enum.sum()
   end
 
@@ -24,22 +15,31 @@ defmodule Advent2023.Day07 do
       |> String.split(" ")
 
     {bid, ""} = Integer.parse(bid_part)
-    hand = String.to_charlist(hand_part)
+    hand = parse_hand(hand_part)
 
     {hand, bid}
+  end
+
+  def part_two(input) do
+    input
+    |> Enum.map(&parse_line/1)
+    |> Enum.map(fn {hand, bid} -> {Enum.map(hand, &rank_card(&1, jacks: :low)), bid} end)
+    |> Enum.sort_by(&classify_hand(&1, jacks: :wildcard))
+    |> Enum.with_index(1)
+    |> Enum.map(fn {{_hand, wager}, i} -> wager * i end)
+    |> Enum.sum()
   end
 
   def parse_hand(hand_part) do
     hand_part
     |> String.to_charlist()
-    |> Enum.map(&rank_card/1)
   end
 
-  def rank_card(card, opts \\ [jacks: :high]) do
+  def rank_card(card, jacks: jacks) do
     jack =
-      case Keyword.fetch!(opts, :jacks) do
-        :high -> 11
+      case jacks do
         :low -> 1
+        :high -> 11
       end
 
     case card do
@@ -59,102 +59,34 @@ defmodule Advent2023.Day07 do
     end
   end
 
-  def most_jacked(
-        {{{{{jacked_classification_rank, _jacked_classification_label}, _jacked_card_rankings},
-           _jacked_hand},
-          {{{_originaL_classification_rank, _oroginal_classification_label},
-            original_card_rankings}, _original_hand}}, _wager}
-      ) do
-    {jacked_classification_rank, original_card_rankings}
-  end
-
-  def find_most_jacked_hand({hand, wager}) do
-    jacked =
-      hand
-      |> generate_hands()
-      |> Enum.map(
-        &{
-          classify_hand(&1, jacks: :low),
-          &1
-        }
-      )
-      |> Enum.sort()
-      |> List.last()
-
-    original =
-      {classify_hand(hand), hand}
-
-    {{jacked, original}, wager}
-  end
-
-  def generate_hands([a, b, c, d, e] = hand)
-      when a != ?J and b != ?J and c != ?J and d != ?J and e != ?J do
-    [hand]
-  end
-
-  def generate_hands(~c"JJJJJ"), do: [~c"AAAAA"]
-
-  def generate_hands(hand) do
-    jacks = Enum.filter(hand, &(&1 == ?J))
-    non_jacks = Enum.reject(hand, &(&1 == ?J))
-
-    replacement_card_groups =
-      non_jacks
-      |> MapSet.new()
-      |> MapSet.to_list()
-      |> replicate(Enum.count(jacks))
-      |> cartesian_product()
-
-    Enum.map(replacement_card_groups, fn replacements ->
-      Enum.reduce(replacements, hand, fn replacement, acc ->
-        replace_first(acc, ?J, replacement)
-      end)
-    end)
-  end
-
-  def cartesian_product([]), do: []
-
-  def cartesian_product(lists) do
-    lists
-    |> Enum.reduce(&cartesian_product/2)
-    |> List.flatten()
-    |> Enum.chunk_every(Enum.count(lists))
-  end
-
-  def cartesian_product(set_a, set_b) do
-    for a <- set_a, b <- set_b, do: [a, b]
-  end
-
-  def classify_hand(hand, opts \\ [jacks: :high]) do
-    ranked_hand = Enum.map(hand, &rank_card(&1, opts))
-
-    classification =
+  def classify_hand({ranked_hand, _bid}, jacks: jacks) do
+    counts =
       ranked_hand
       |> Enum.frequencies()
+      |> handle_wildcards(jacks)
       |> Map.values()
       |> Enum.sort()
-      |> classify_counts()
 
-    {classification, ranked_hand}
-  end
-
-  def classify_counts(counts) do
-    # return the counts here
     case counts do
-      [5] -> {7, :five_of_kind}
-      [1, 4] -> {6, :four_of_kind}
-      [2, 3] -> {5, :full_house}
-      [1, 1, 3] -> {4, :three_of_kind}
-      [1, 2, 2] -> {3, :two_pair}
-      [1, 1, 1, 2] -> {2, :one_pair}
-      [1, 1, 1, 1, 1] -> {1, :high_card}
+      [5] -> {7, :five_of_kind, ranked_hand}
+      [1, 4] -> {6, :four_of_kind, ranked_hand}
+      [2, 3] -> {5, :full_house, ranked_hand}
+      [1, 1, 3] -> {4, :three_of_kind, ranked_hand}
+      [1, 2, 2] -> {3, :two_pair, ranked_hand}
+      [1, 1, 1, 2] -> {2, :one_pair, ranked_hand}
+      [1, 1, 1, 1, 1] -> {1, :high_card, ranked_hand}
     end
   end
 
-  def replicate(_, 0), do: []
-  def replicate(x, 1), do: [x]
-  def replicate(x, n), do: [x | replicate(x, n - 1)]
-  def replace_first([], _x, _y), do: []
-  def replace_first([x | xs], x, y), do: [y | xs]
-  def replace_first([h | xs], x, y), do: [h | replace_first(xs, x, y)]
+  def handle_wildcards(frequencies, :normal), do: frequencies
+
+  def handle_wildcards(frequencies, :wildcard) do
+    most_frequent =
+      frequencies |> Enum.max_by(fn {_, v} -> v end) |> elem(0)
+
+    jack_frequency = Map.get(frequencies, ?J, 0)
+
+    %{frequencies | most_frequent => frequencies[most_frequent] + jack_frequency}
+    |> Map.delete(?J)
+  end
 end
--- test/advent2023/day_07_test.exs
+++ test/advent2023/day_07_test.exs
@@ -11,36 +11,13 @@ defmodule Advent2023.Day07Test do
   """
   describe "part_one/1" do
     test "solves the sample correctly" do
-      # solved =
-      #   @sample
-      #   |> String.trim()
-      #   |> String.split("\n")
-      #   |> Day07.part_one()
+      solved =
+        @sample
+        |> String.trim()
+        |> String.split("\n")
+        |> Day07.part_one()
 
-      # # assert solved == 6440
-    end
-  end
-
-  describe "cartesian_product/1" do
-    test "returns the cartesian product of the lists in the list" do
-      assert Day07.cartesian_product([[1, 2], [1, 2], [1, 2]]) ==
-               [
-                 [1, 1, 1],
-                 [1, 1, 2],
-                 [1, 2, 1],
-                 [1, 2, 2],
-                 [2, 1, 1],
-                 [2, 1, 2],
-                 [2, 2, 1],
-                 [2, 2, 2]
-               ]
-    end
-  end
-
-  describe "cartesian_product/2" do
-    test "returns the cartesian product of the 2 lists" do
-      product_1 = [[1, 1], [1, 2], [2, 1], [2, 2]]
-      assert Day07.cartesian_product([1, 2], [1, 2]) == product_1
+      assert solved == 6440
     end
   end
 end
