0ca2ef064807342c5f05dfcd4fc174868a5169e0
shawa <shawa@localhost>
2023-12-09 18:02:45 +0000

    clean

--- lib/advent2023.ex
+++ lib/advent2023.ex
@@ -3,35 +3,14 @@ defmodule Advent2023 do
   Documentation for `Advent2023`.
   """
 
-  alias Advent2023.Helpers
-
-  @spec run!() :: :ok
-  def run! do
+  def run() do
     {:ok, all_modules} =
       :application.get_key(:advent_2023, :modules)
 
     all_modules
-    |> Enum.map(&Atom.to_string/1)
-    |> Enum.filter(&match?("Elixir.Advent2023.Day" <> <<_day::binary-size(2)>>, &1))
-    |> Enum.map(&String.to_existing_atom/1)
-    |> Enum.each(&run/1)
+    |> Enum.filter(fn module ->
+      match?(<<"Elixir.Advent2023.Day", _::binary-size(2)>>, Atom.to_string(module))
+    end)
+    |> Enum.map(&{&1, apply(&1, :run, [])})
   end
-
-  @spec run(module()) :: :ok
-  def run(day) do
-    input_lines = input_format_for(day).(day)
-
-    part_1_result = day.part_one(input_lines)
-    part_2_result = day.part_two(input_lines)
-
-    IO.puts("""
-    # #{inspect(day)}
-
-    Part 1: #{part_1_result}
-    Part 2: #{part_2_result}
-    """)
-  end
-
-  def input_format_for(Advent2023.Day05), do: &Helpers.input_for/1
-  def input_format_for(_), do: &Helpers.input_lines_for/1
 end
--- lib/advent2023/day.ex
+++ lib/advent2023/day.ex
@@ -1,29 +1,25 @@
 defmodule Advent2023.Day do
+  alias Advent2023.Input
+
   defmacro __using__(opts) do
     input_format = Keyword.get(opts, :input)
 
     quote do
-      def input(part_or_sample \\ 1) do
+      def input(input_or_sample \\ :input) do
         unquote(
           if input_format == :lines do
             quote do
-              Advent2023.Helpers.input_lines_for(__MODULE__, part_or_sample)
+              Input.input_lines_for(__MODULE__, input_or_sample)
             end
           else
             quote do
-              Advent2023.Helpers.input_for(__MODULE__, part_or_sample)
+              Input.input_for(__MODULE__, input_or_sample)
             end
           end
         )
       end
 
-      def run(part_or_sample \\ 1) do
-        format_atom =
-          fn
-            :part_one -> "Part 1"
-            :part_two -> "Part 2"
-          end
-
+      def run(input_or_sample \\ :input) do
         mfas =
           [
             {__MODULE__, :part_one, 1},
@@ -35,7 +31,7 @@ defmodule Advent2023.Day do
           function_exported?(module, function, arity)
         end)
         |> Enum.map(fn {module, function, _arity} ->
-          {function, apply(module, function, [input(part_or_sample)])}
+          {function, apply(module, function, [input(input_or_sample)])}
         end)
       end
     end
